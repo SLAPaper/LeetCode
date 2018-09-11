4d5c774a724d9fdd937bc781cf283701945e056f
SLAPaper <slapaper.pku@gmail.com>
2018-09-12 02:04:26 +0800

    valid palindrome finished

--- Explore - Top Interview Questions (Easy)/Strings/5 Valid Palindrome/solution.cpp
+++ Explore - Top Interview Questions (Easy)/Strings/5 Valid Palindrome/solution.cpp
@@ -1,16 +1,41 @@
 #include <string>
 #include <iostream>
+#include <cctype>
 
 using namespace std;
 
 class Solution {
 public:
     bool isPalindrome(string s) {
+        int size = s.size();
+        int p = 0;
+        int q = size - 1;
+        while (p < q) {
+            if (!isdigit(s[p]) && !isalpha(s[p])) {
+                ++p;
+                continue;
+            }
+            if (!isdigit(s[q]) && !isalpha(s[q])) {
+                --q;
+                continue;
+            }
+            if (tolower(s[p]) != tolower(s[q])) {
+                return false;
+            }
+            ++p;
+            --q;
+        }
         return true;
     }
 };
 
 int main() {
-    cout << "Hello World!";
+    Solution s;
+    if (s.isPalindrome("")) {
+        cout << "true" << endl;
+    }
+    else {
+        cout << "false" << endl;
+    }
     return 0;
 }
